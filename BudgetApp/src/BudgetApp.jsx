c996e01b2dbec9c08cbb3260016cbe3f7e65a294
kwrona05 <kwrona05@gmail.com>
2024-09-24 14:13:54 +0200

    create main file

--- BudgetApp/src/BudgetApp.jsx
+++ BudgetApp/src/BudgetApp.jsx
@@ -1,2 +1,30 @@
-function BudgetApp() {}
+import { useState } from "react";
+import TransactionForm from "./TransactionForm";
+import BudgetSummary from "./BudgetSummary";
+import FoundsList from "./FoundsList";
+
+function BudgetApp() {
+  const [transactions, setTransactions] = useState([]);
+
+  const addTransaction = (transaction) => {
+    setTransactions([...transactions, transaction]);
+  };
+
+  const deleteTransactions = (index) => {
+    const newTransactions = transactions.filter((_, i) => i !== index);
+    setTransactions(newTransactions);
+  };
+
+  return (
+    <div className="container">
+      <h1>Budget Menager App</h1>
+      <TransactionForm addTransaction={addTransaction} />
+      <BudgetSummary transactions={transactions} />
+      <FoundsList
+        transactions={transactions}
+        deleteTransactions={deleteTransactions}
+      />
+    </div>
+  );
+}
 export default BudgetApp;
